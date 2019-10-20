68fde1b731952cefb7caf37150f0af7f28081c75
Swang24t <49329520+Swang24t@users.noreply.github.com>
2019-10-20 01:32:04 -0700

    Update HW2.py

--- HW2.py
+++ HW2.py
@@ -2,12 +2,12 @@ import pandas as pd
 df = pd.read_csv("https://data.seattle.gov/api/views/tw7j-dfaw/rows.csv")
 
 def test_create_dataframe(df, columnName):
-    for col in df.columns:  # first condition
+    for col in df.columns:  # first condition check if in list 
         if col not in columnName:
             return false
-    for b in df.dtypes:
-        if b != object:
+    for b in df.dtypes:#determine if same 
+        if b == object:
             return false
-    if df.shape[1] < 10:  # third one
+    if df.shape[1] < 10:  # third one determine the size
         return false
     return true
